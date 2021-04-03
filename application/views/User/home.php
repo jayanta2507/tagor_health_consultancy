647c140d15b2fa23113aebf63483228d319ccfbb
Ayan Kumar Saha <ayansaha289@gmail.com>
2021-04-03 10:29:02 +0530

    upload_login

---
+++ application/views/User/home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--Code by Divinector (www.divinectorweb.com)-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tagor Health Consultancy</title>
+   <div style="background-image:url('images/2.jpg');">
+    <link rel="stylesheet" href="style1.css">    
+</head>
+<body>
+
+    
+    <header>
+    <div class="wrapper">
+        <div class="logo">
+            <img src="logo.jpeg" alt="">
+          
+        </div>
+<ul class="nav-area">
+<li><a href="#">Home</a></li>
+<li><a href="#">About Us</a></li>
+<li><a href="#">Facilites</a></li>
+<li><a href="#">Contact Us</a></li>
+<li><a href="regester.php">Registration</a></li>
+<li><a href="#">Location</a></li>
+<input type ="text" placeholder="Search..">
+</ul>
+</div><p>
+
+</header>
+
+
+</body>
+</html>
