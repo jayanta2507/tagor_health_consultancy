adbbc93ab62bc9b380fd6eee84bac32d864c10cc
Swastika Saha <swastikas9874@gmail.com>
2021-06-14 13:35:30 +0530

    upload_bed

--- application/config/database.php
+++ application/config/database.php
@@ -74,10 +74,10 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	   => '',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => 'user123',
+	'password' => '',
 	'database' => 'tagor_consultancy',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
